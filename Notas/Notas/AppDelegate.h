8a4a674d18ead3635f7f220c7ff62e03e4064a9f
Eduardo Santi <eduardo.senacrs@gmail.com>
2015-03-05 16:20:13 -0300

    init commit

---
+++ Notas/Notas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notas
+//
+//  Created by Eduardo Santi on 05/03/15.
+//  Copyright (c) 2015 Eduardo Santi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
